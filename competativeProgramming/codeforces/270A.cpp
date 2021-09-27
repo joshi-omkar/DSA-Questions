a200107c9584a8532652f0e05be08652ee9d4d8f
Omkar Joshi <joshiomkar0400@gmail.com>
2021-09-27 12:44:49 +0530

    initial commit

---
+++ competativeProgramming/codeforces/270A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        if (360 % (180 - a) == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+
